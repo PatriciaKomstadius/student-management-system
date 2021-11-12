7d460caa7b2961158b171f097c8189e67ce3ed35
Patricia K <patricia.komstadius@gmail.com>
2021-11-12 15:18:09 +0100

    Methods in restclass created

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,66 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("students")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
+
+    @Inject
+    StudentService studentService;
+
+    //POST
+    @Path("")
+    @POST
+    public Response createStudent(Student student) {
+        studentService.createStudent(student);
+        return Response.ok(student).build();
+    }
+
+
+    //GET one
+
+    @Path("{id}")
+    @GET
+    public Response getStudent(@PathParam("id") Long id) {
+        Student foundStudent = studentService.findStudentById(id);
+
+        return Response.ok(foundStudent).build();
+
+    }
+
+    //GET all
+    @Path("")
+    @GET
+    public Response getAllStudents(){
+        List<Student> allStudents = studentService.getStudents();
+        return Response.ok(allStudents).build();
+    }
+
+    //PUT
+    @Path("")
+    @PUT
+    public Response updateStudent(Student student){
+        studentService.updateStudent(student);
+        return Response.ok(student).build();
+    }
+
+
+    //DELETE
+    @Path("{id}")
+    @DELETE
+    public Response deleteStudent(@PathParam("id") Long id) {
+        studentService.deleteStudent(id);
+        return Response.ok().build();
+    }
 
 
 }
