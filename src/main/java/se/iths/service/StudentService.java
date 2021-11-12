8dd123a785a888bf39d0bf51354440fa48b67708
Patricia K <patricia.komstadius@gmail.com>
2021-11-12 14:09:01 +0100

    Methods in service created

--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,41 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
 public class StudentService {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    //CREATE
+    public Student createStudent(Student student) {
+        entityManager.persist(student);
+        return student;
+    }
+
+    //READ
+    public List<Student> getStudents() {
+        return entityManager.createQuery("SELECT i FROM Student i", Student.class).getResultList();
+    }
+
+    //UPDATE
+    public Student updateStudent(Student student) {
+        entityManager.merge(student);
+        return student;
+    }
+
+    //DELETE
+    public void deleteStudent(Long id) {
+        Student studentToDelete = entityManager.find(Student.class, id);
+        entityManager.remove(studentToDelete);
+    }
+
 
 
 }
