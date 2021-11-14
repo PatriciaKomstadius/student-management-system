549df97b6c2b5df8592f000aebc124e5d504a396
Patricia K <patricia.komstadius@gmail.com>
2021-11-14 22:05:26 +0100

    ErrorMessage class for handling exception messages created

---
+++ src/main/java/se/iths/exception/ErrorMessage.java
@@ -0,0 +1,32 @@
+package se.iths.exception;
+
+public class ErrorMessage {
+
+    private int errorCode;
+    private String errorMessage;
+
+    public ErrorMessage(int errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+
+
+
+}
